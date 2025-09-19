0677f1e97ae1cf0e8e48fca10f39229c585c9cac
DaniReiis <danielreis350@gmail.com>
2025-09-19 16:20:40 -0300

    criando o arquivo ft-indo-bem

--- themes/halley/template-parts/ft-conheca.php
+++ themes/halley/template-parts/ft-conheca.php
@@ -4,29 +4,29 @@
         <p class="text-center text-primary">A Halley é uma empresa que atua na logística e transporte de diferentes tipos de carga: material biológico, medicamentos e carga seca. Nossos clientes sabem todos os dias que, se a remessa tá indo de Halley, <strong>tá indo bem.</strong></p>
         <div class="row justify-content-center mt-5">
             <div class="col-lg-3">
-                <div class="bg-secondary h-100 text-white py-4 px-5 d-flex flex-column justify-content-center align-items-center">
+                <div class="bg-secondary h-100 rounded-4 text-white py-4 px-5 d-flex flex-column justify-content-center align-items-center">
                     <span class="fs-6">Conheça nossos serviços</span>
                     <p class="fw-bolder fs-4">Logística, Armazenagem e Transporte</p>
                 </div>
             </div>
 
             <div class="col-lg-3">
-                <div class="bg-primary text-center h-100 text-white p-4 px-5 d-flex flex-column justify-content-center align-items-center">
-                    <i class="fa-solid fa-circle-radiation fs-1 mb-3"></i>
+                <div class="bg-primary text-center h-100 rounded-4 text-white p-4 px-5 d-flex flex-column justify-content-center align-items-center">
+                    <img src="<?php echo wp_get_attachment_url('23'); ?>" class="fs-1 mb-3" alt="">
                     <p class="fw-bolder fs-4">Transporte de Material Biológico</p>
                 </div>
             </div>
 
              <div class="col-lg-3">
-                <div class="bg-primary text-center h-100 text-white p-4 px-5 d-flex flex-column justify-content-center align-items-center">
-                    <i class="fa-solid fa-briefcase-medical fs-1 mb-3"></i>
+                <div class="bg-primary text-center h-100 rounded-4 text-white p-4 px-5 d-flex flex-column justify-content-center align-items-center">
+                    <img src="<?php echo wp_get_attachment_url('25'); ?>" class="fs-1 mb-3" alt="">
                     <p class="fw-bolder fs-4">Transporte de Produtos Farmacêuticos</p>
                 </div>
             </div>
 
              <div class="col-lg-3">
-                <div class="bg-primary text-center h-100 text-white p-4 px-5 d-flex flex-column justify-content-center align-items-center">
-                    <i class="fa-solid fa-box-open fs-1 mb-3"></i>
+                <div class="bg-primary text-center h-100 rounded-4 text-white p-4 px-5 d-flex flex-column justify-content-center align-items-center">
+                    <img src="<?php echo wp_get_attachment_url('24'); ?>" class="fs-1 mb-3" alt="">
                     <p class="fw-bolder fs-4">Transporte de Carga Seca</p>
                 </div>
             </div>
--- themes/halley/template-parts/ft-indo-bem.php
+++ themes/halley/template-parts/ft-indo-bem.php
@@ -1,1 +1,38 @@
-i
+<section class="ft-indo-bem bg-primary u-py-5">
+    <div class="container">
+        <h2 class="text-white">Por que tá indo bem?</h2>
+        <p class="text-white">Hospitais, laboratórios, armazéns, portos e estradas. Independente do local, essas são algumas razões da sua carga ir sempre bem com a Halley.</p>
+
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="border p-5 rounded-4 text-center border-light-subtle d-flex flex-column justify-content-center align-items-center h-100">
+                    <img src="<?php echo wp_get_attachment_url('29'); ?>" alt="" class="mb-3">
+                    <p class="text-white">Certificação Anvisa</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="border p-5 rounded-4 text-center border-light-subtle d-flex flex-column justify-content-center align-items-center h-100">
+                    <img src="<?php echo wp_get_attachment_url('28'); ?>" alt="" class="mb-3">
+                    <p class="text-white">Veículos Revestidos</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="border p-5 rounded-4 text-center border-light-subtle d-flex flex-column justify-content-center align-items-center h-100">
+                    <img src="<?php echo wp_get_attachment_url('27'); ?>" alt="" class="mb-3">
+                    <p class="text-white">Carga Seca ou Refrigerada</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="border p-5 rounded-4 text-center border-light-subtle d-flex flex-column justify-content-center align-items-center h-100">
+                    <img src="<?php echo wp_get_attachment_url('26'); ?>" alt="" class="mb-3">
+                    <p class="text-white">Armazenagem Segura</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
