9eddb7dd34b5428ebf022fc7c90a25370b545cb4
Matheus Lima <matheus.lima@planetaw.ag>
2025-09-30 16:08:39 -0300

    finalizando o footer

--- themes/halley/footer.php
+++ themes/halley/footer.php
@@ -1,7 +1,48 @@
+
+
+<section class="rodape">
+        <div class="rodape__links">
+            <img src="<?php echo wp_get_attachment_url('33'); ?>" alt="">
+            <ul class="rodape__sobre">
+                <h3 class="rodape__topico">Sobre nós</h3>
+                <li class="rodape_topicos_links"><a href="#">Fale com a Halley</a></li>
+                <li class="rodape_topicos_links"><a href="#">Logística</a></li>
+                <li class="rodape_topicos_links"><a href="#">Somos Halley</a></li>
+            </ul>
+
+            <ul class="rodape__sobre2">
+                <h3 class="rodape__topico">Principais Serviços</h3>
+                <li class="rodape_topicos_links"><a href="#">Transporte de Material Biológico</a></li>
+                <li class="rodape_topicos_links"><a href="#">Transporte de Produtos Farmacêuticos</a></li>
+                <li class="rodape_topicos_links"><a href="#">Transporte de Carga Seca</a></li>
+            </ul>
+
+            <ul class="rodape__sobre">
+                <h3 class="rodape__topico">Horário de Funcionamento</h3>
+                <li class="rodape_topicos_links">Segunda a Sexta: 8:00 - 18:00</li>
+                <li class="rodape_topicos_links">Plantão aos finais de semana e feriados</li>
+            </ul>
+        </div>
+    </section>
+
+    
+
 <footer id="footer-site">
-    <div class="container">
-        footer
-    </div>
+    <div class="footer__container">
+            <div class="footer__topicos">
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+
+            <p class="footer__copyright">© Copyright Halley - Todos os direitos reservados</p>
+            <div class="footer__planeta">
+                <a href="#">
+                    <img src="<?php echo wp_get_attachment_url('34'); ?>" alt="Logo Planeta">
+                </a>
+            </div>
+        </div>
 
 </footer>
 </div><!-- WRAPPER SITE -->
