08e3ae926760f9742e8e31197b533b86444ad0c7
Matheus Lima <matheus.lima@planetaw.ag>
2025-10-01 10:09:51 -0300

    criação de seção contatos

--- themes/halley/template-parts/ft-dados-contato.php
+++ themes/halley/template-parts/ft-dados-contato.php
@@ -1,1 +1,22 @@
-d
+<section class="contato">
+    <div class="contato__topicos">
+        <div class="contato__dados">
+            <img src="<?php echo wp_get_attachment_url('35'); ?>" alt="">
+            <h3 class="contato__titulo">Ponto de Partida</h3>
+            <p class="contato__texto">Rua Odorico Mendes, 491 <br>
+            Mooca • São Paulo - SP - 03106-030</p>
+        </div>
+        <div class="contato__dados">
+            <img src="<?php echo wp_get_attachment_url('36'); ?>" alt="">
+            <h3 class="contato__titulo">Alô, como tá indo?</h3>
+            <p class="contato__texto">+55 11 5034 5555 <br>
+            +55 11 2271 0030</p>
+        </div>
+        <div class="contato__dados">
+            <img src="<?php echo wp_get_attachment_url('37'); ?>" alt="">
+            <h3 class="contato__titulo">Já Chegou!</h3>
+            <p class="contato__texto">atendimento@halleylog.com.br</p>
+        </div>
+    </div>
+</section>
+
