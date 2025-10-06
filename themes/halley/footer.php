5c43b89b752e0075a876075b1d6c0d6e21b33061
BrandsDev <lucasbrandao392@gmail.com>
2025-10-06 12:55:41 -0300

    Ajustando as brincadeiras

--- themes/halley/footer.php
+++ themes/halley/footer.php
@@ -1,48 +1,55 @@
 
+<footer>
 
-<section class="rodape">
-        <div class="rodape__links">
-            <img src="<?php echo wp_get_attachment_url('33'); ?>" alt="">
-            <ul class="rodape__sobre">
-                <h3 class="rodape__topico">Sobre nós</h3>
-                <li class="rodape_topicos_links"><a href="#">Fale com a Halley</a></li>
-                <li class="rodape_topicos_links"><a href="#">Logística</a></li>
-                <li class="rodape_topicos_links"><a href="#">Somos Halley</a></li>
-            </ul>
+    <section class="bg-primary u-py-5">
+        <div class="container">
+            <div class="row justify-content-between gy-4">
 
-            <ul class="rodape__sobre2">
-                <h3 class="rodape__topico">Principais Serviços</h3>
-                <li class="rodape_topicos_links"><a href="#">Transporte de Material Biológico</a></li>
-                <li class="rodape_topicos_links"><a href="#">Transporte de Produtos Farmacêuticos</a></li>
-                <li class="rodape_topicos_links"><a href="#">Transporte de Carga Seca</a></li>
-            </ul>
+                <div class="col-md-6 col-lg-3">
+                    <img src="<?php echo wp_get_attachment_url('33'); ?>" alt="">
+                </div>
 
-            <ul class="rodape__sobre">
-                <h3 class="rodape__topico">Horário de Funcionamento</h3>
-                <li class="rodape_topicos_links">Segunda a Sexta: 8:00 - 18:00</li>
-                <li class="rodape_topicos_links">Plantão aos finais de semana e feriados</li>
-            </ul>
+                <div class="col-md-6 col-lg-2 text-white">
+                    <h3 class="mb-4">Sobre nós</h3>
+                    <p class="small m-0"><a href="#" class="link-light text-decoration-none">Fale com a Halley</a></p>
+                    <p class="small m-0"><a href="#" class="link-light text-decoration-none">Logística</a></p>
+                    <p class="small m-0"><a href="#" class="link-light text-decoration-none">Somos Halley</a></p>
+                </div>
+
+                <div class="col-md-6 col-lg-3 text-white">
+                    <h3 class="mb-4">Principais Serviços</h3>
+                    <p class="small m-0"><a href="#" class="link-light text-decoration-none">Transporte de Material Biológico</a></p>
+                    <p class="small m-0"><a href="#" class="link-light text-decoration-none">Transporte de Produtos Farmacêuticos</a></p>
+                    <p class="small m-0"><a href="#" class="link-light text-decoration-none">Transporte de Carga Seca</a></p>
+                </div>
+
+                <div class="col-md-6 col-lg-3 text-white">
+                    <h3 class="mb-4">Horário de Funcionamento</h3>
+                    <p class="small m-0">Segunda a Sexta: 8:00 - 18:00</p>
+                    <p class="small m-0">Plantão aos finais de semana e feriados</p>
+                </div>
+
+            </div>
         </div>
     </section>
 
     
 
-<footer id="footer-site">
-    <div class="footer__container">
-            <div class="footer__topicos">
-                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
-                <a href="#"><i class="fa-brands fa-youtube"></i></a>
-            </div>
-
-            <p class="footer__copyright">© Copyright Halley - Todos os direitos reservados</p>
-            <div class="footer__planeta">
-                <a href="#">
-                    <img src="<?php echo wp_get_attachment_url('34'); ?>" alt="Logo Planeta">
-                </a>
+    <div class="bg-secondary py-2">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center flex-wrap">
+                <div class="btn-rs-white">
+                    <?= get_template_part('template-parts/c-redes-sociais'); ?>
+                </div>
+                <p class="text-white text-center m-0">© Copyright Halley - Todos os direitos reservados</p>
+                <div class="footer__planeta">
+                    <a href="https://planetaw.ag" target="_blank" rel="noopener noreferrer" title="Planeta W">
+                        <img src="<?= wp_get_attachment_url('34'); ?>" alt="Logo Planeta">
+                    </a>
+                </div>
             </div>
         </div>
+    </div>
 
 </footer>
 </div><!-- WRAPPER SITE -->
--- themes/halley/template-parts/ft-dados-contato.php
+++ themes/halley/template-parts/ft-dados-contato.php
@@ -1,15 +1,14 @@
 <section class="contato u-py-5">
     <div class="container">
         <div class="row justify-content-between gy-4">
-            <div class="col-lg-3 text-center ">
+            <div class="col-md-6 col-lg-3 text-center ">
                 <div class="d-flex justify-content-center align-itens-center rounded-4 bg-secondary p-4 mx-auto" style="width: 75px; height: 75px;">
                     <img src="<?= wp_get_attachment_url('35'); ?>" class="" alt="">
                 </div>
                 <h3 class="text-secondary mt-4">Ponto de Partida</h3>
-                <p class="text-primary">Rua Odorico Mendes, 491 <br>
-                Mooca • São Paulo - SP - 03106-030</p>
+                <p class="text-primary">Rua Odorico Mendes, 491 <br> Mooca • São Paulo - SP - 03106-030</p>
             </div>
-            <div class="col-lg-3 text-center ">
+            <div class="col-md-6 col-lg-3 text-center ">
                 <div class="d-flex justify-content-center align-itens-center rounded-4 bg-secondary p-4 mx-auto" style="width: 75px; height: 75px;">
                     <img src="<?= wp_get_attachment_url('38'); ?>" class="" alt="">
                 </div>
@@ -17,7 +16,7 @@
                 <p class="text-primary">+55 11 5034 5555 <br>
                 +55 11 2271 0030</p>
             </div>
-            <div class="col-lg-3 text-center ">
+            <div class="col-md-6 col-lg-3 text-center ">
                 <div class="d-flex justify-content-center align-itens-center rounded-4 bg-secondary p-4 mx-auto" style="width: 75px; height: 75px;">
                     <img src="<?= wp_get_attachment_url('37'); ?>" class="" alt="">
                 </div>
