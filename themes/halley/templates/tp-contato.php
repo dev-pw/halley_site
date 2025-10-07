a2c3d109343404b10d8eddb0ef4dd1fb02b891a8
Brandão <lucas.brandao@planetaw.ag>
2025-10-07 16:46:47 -0300

    Subindo ajustes de páginas

--- themes/halley/templates/tp-contato.php
+++ themes/halley/templates/tp-contato.php
@@ -3,7 +3,7 @@
  * Template Name: Contato
  * Template Post Type: page
  * @package WordPress
- * @subpackage sbrvthememedico
+ * @subpackage halley-site
  *
 */
 
@@ -31,9 +31,9 @@ $email = get_option('options_info_1');
             <div class="row gy-4 justify-content-center">
 
                 <div class="col-lg-4">
-                    <div class="border border-4 bg-light rounded p-4 d-flex flex-column text-center align-items-center h-100">
+                    <div class="border border-4 border-primary bg-light rounded p-4 d-flex flex-column text-center align-items-center h-100">
                         <span class="fs-2 icon-location_on mb-3 text-primary"></span>
-                        <h2 class="h5 mb-3 text-primary">Endereço</h2>
+                        <h2 class="h5 mb-3 text-primary">Ponto de Partida</h2>
                         <a class="text-decoration-none small" href="<?= $maps; ?>" target="_blank" title="Localização"><?= $endereco; ?></a>
                         <div class="row gx-2 gx-md-4 mt-2">
                             <div class="col-6">
@@ -47,7 +47,7 @@ $email = get_option('options_info_1');
                 </div>
 
                 <div class="col-lg-4">
-                    <div class="border border-4 bg-light rounded p-4 d-flex flex-column text-center align-items-center h-100">
+                    <div class="border border-4 border-primary bg-light rounded p-4 d-flex flex-column text-center align-items-center h-100">
                         <span class="fs-2 icon-call mb-3 text-primary"></span>
                         <h2 class="h5 mb-3 text-primary">Fale Conosco</h2>
                         <div>
@@ -60,7 +60,7 @@ $email = get_option('options_info_1');
                 </div>
 
                 <div class="col-lg-4">
-                    <div class="border border-4 bg-light rounded p-4 d-flex flex-column text-center align-items-center h-100">
+                    <div class="border border-4 border-primary bg-light rounded p-4 d-flex flex-column text-center align-items-center h-100">
                         <span class="fs-2 icon-mail-envelope-open mb-3 text-primary"></span>
                         <h2 class="h5 mb-3 text-primary">E-mail</h2>
                         <div>
@@ -71,9 +71,9 @@ $email = get_option('options_info_1');
 
             </div>
 
-            <div class="bg-light flex-column-reverse flex-xl-row mt-5 mx-0 rounded-3 row border border-4">
+            <div class="bg-light flex-column-reverse flex-xl-row mt-5 mx-0 rounded-3 row border border-4 border-primary">
                 <div class="col-xl-5 p-0 s-mapa-contato">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.3386628485155!2d-46.61841615817891!3d-23.55627736860583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59f9adb65883%3A0xe37da38c315d2e10!2sHalley%20Log%C3%ADstica%20e%20Transporte%20LTDA!5e0!3m2!1spt-BR!2sbr!4v1759862740506!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.3386628485155!2d-46.61841615817891!3d-23.55627736860583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59f9adb65883%3A0xe37da38c315d2e10!2sHalley%20Log%C3%ADstica%20e%20Transporte%20LTDA!5e0!3m2!1spt-BR!2sbr!4v1759862740506!5m2!1spt-BR!2sbr" width="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div class="col-xl-7">
                     <article class="p-xl-5 px-3 py-4">
@@ -97,6 +97,23 @@ $email = get_option('options_info_1');
                                         <input type="text" class="form-control border-primary text-dark rounded js_tel_mask" id="campo-telefone" placeholder="telefone" name="telefone" maxlength="15" wfd-id="id2">
                                         <label for="campo-telefone" class="fw-bold small text-primary form-label">Telefone *</label>
                                     </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-floating mb-3">
+                                        <input type="text" class="form-control border-primary text-dark rounded" id="campo-empresa" placeholder="Empresa" name="empresa" wfd-id="id1">
+                                        <label for="campo-empresa" class="fw-bold small text-primary form-label">Empresa</label>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                  <div class="form-floating mb-3">
+                                    <select class="form-select border-primary" id="floatingSelect" aria-label="Floating label select example">
+                                      <option selected> -- Selecione a opção --</option>
+                                      <option value="Logística">Logística</option>
+                                      <option value="Transporte">Transporte</option>
+                                      <option value="Armazenagem">Armazenagem</option>
+                                    </select>
+                                    <label for="floatingSelect" class="text-primary">Tipo de Serviço:</label>
+                                  </div>
                                 </div>
                                 <div class="col">
                                     <div class="form-floating mb-3">
