ce6274da99e0d0843d4907553cf31fef220ed29c
Matheus Lima <matheus.lima@planetaw.ag>
2025-10-07 16:02:22 -0300

    ajustado seção de certificações e footer

--- themes/halley/footer.php
+++ themes/halley/footer.php
@@ -3,24 +3,36 @@
 
     <section class="bg-primary u-py-5">
         <div class="container">
-            <div class="row justify-content-between gy-4">
+            <div class="row justify-content-between gy-4 d-flex flex-column flex-xl-row align-items-center align-items-xl-start text-center text-xl-start">
 
-                <div class="col-md-6 col-lg-3">
-                    <img src="<?php echo wp_get_attachment_url('33'); ?>" alt="">
+                <div class="col-md-6 col-xl-3">
+                    <img src="<?php echo wp_get_attachment_url('33'); ?>" alt="" class="d-block mx-auto w-50 mb-4">
                 </div>
 
                 <div class="col-md-6 col-lg-2 text-white">
                     <h3 class="mb-4">Sobre nós</h3>
-                    <p class="small m-0"><a href="#" class="link-light text-decoration-none">Fale com a Halley</a></p>
-                    <p class="small m-0"><a href="#" class="link-light text-decoration-none">Logística</a></p>
-                    <p class="small m-0"><a href="#" class="link-light text-decoration-none">Somos Halley</a></p>
+                    <p class="small m-0">
+                        <a href="#" class="link-light text-decoration-none">Fale com a Halley</a>
+                    </p>
+                    <p class="small m-0">
+                        <a href="#" class="link-light text-decoration-none">Logística</a>
+                    </p>
+                    <p class="small m-0">
+                        <a href="#" class="link-light text-decoration-none">Somos Halley</a>
+                    </p>
                 </div>
 
                 <div class="col-md-6 col-lg-3 text-white">
                     <h3 class="mb-4">Principais Serviços</h3>
-                    <p class="small m-0"><a href="#" class="link-light text-decoration-none">Transporte de Material Biológico</a></p>
-                    <p class="small m-0"><a href="#" class="link-light text-decoration-none">Transporte de Produtos Farmacêuticos</a></p>
-                    <p class="small m-0"><a href="#" class="link-light text-decoration-none">Transporte de Carga Seca</a></p>
+                    <p class="small m-0">
+                        <a href="#" class="link-light text-decoration-none">Transporte de Material Biológico</a>
+                    </p>
+                    <p class="small m-0">
+                        <a href="#" class="link-light text-decoration-none">Transporte de Produtos Farmacêuticos</a>
+                    </p>
+                    <p class="small m-0">
+                        <a href="#" class="link-light text-decoration-none">Transporte de Carga Seca</a>
+                    </p>
                 </div>
 
                 <div class="col-md-6 col-lg-3 text-white">
@@ -31,6 +43,7 @@
 
             </div>
         </div>
+
     </section>
 
     
