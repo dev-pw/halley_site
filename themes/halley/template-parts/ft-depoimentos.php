0301f8a3058e87d1441ce9f11ed450c7fe2ce0e9
Brandão <lucas.brandao@planetaw.ag>
2025-10-14 16:02:42 -0300

    ft depoimentos

--- themes/halley/template-parts/ft-depoimentos.php
+++ themes/halley/template-parts/ft-depoimentos.php
@@ -1,1 +1,15 @@
-d
+<section class="u-py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 text-end">
+                <h2 class="small"> Depoimentos </h2>
+                <p class="fs-5 fw-bold"> Quem conhece, recomenda </p>
+            </div>
+
+            <div class="col-lg-10">
+                <?= do_shortcode('[trustindex no-registration=google]'); ?>
+            </div>
+
+        </div>
+    </div>
+</section>
