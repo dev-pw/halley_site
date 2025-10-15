63dbd5eaa1a2a1636b2233a449fd0bca9ae5fd27
Brandão <lucas.brandao@planetaw.ag>
2025-10-15 10:40:36 -0300

    Ajustando card de post

--- themes/halley/template-parts/c-card-post.php
+++ themes/halley/template-parts/c-card-post.php
@@ -1,26 +1,44 @@
 <?php
 
-$img = has_post_thumbnail() ? get_post_thumbnail_id($post->ID) : 59;
+$categories = get_the_category();
+$cat = '';
+
+if (count($categories) > 1) {
+    for ($i = 0; $i < count($categories); $i++) {
+        $cat .= $categories[$i]->cat_name;
+        if ($i < count($categories) - 1) {
+            $cat .= ', ';
+        }
+    }
+} else {
+    $cat = $categories[0]->cat_name;
+}
+has_post_thumbnail() ? $img = get_post_thumbnail_id($post->ID) : $img = 59;
 
 ?>
 
-<article <?= post_class(); ?>>
-                    
-    <img src="<?= wp_get_attachment_image_src($img, 'full')[0]; ?>" class="rounded-3 mb-4 img-fluid w-100" alt="<?= the_title(); ?>">
+<article <?= post_class('h-100 shadow-sm rounded-3'); ?>>
+    <figure class="mb-0">
+        <?= wp_get_attachment_image( $img, 'full', '', ["class" => "rounded-3 mb-4 img-fluid w-100"]) ?>
+    </figure>
 
-    <div class="text-secondary small mb-3">
-        <span> <?= get_the_date('d \d\e m, Y'); ?> </span>
-        <span> <?= the_category(); ?> </span>
-    </div>
-    
-    <h2 class="h5">
-        <a href="<?php the_permalink(); ?>" class="text-decoration-none">
-            <?= the_title(); ?>
+    <div class="pt-2 pb-4 px-3">
+        <div class="row gx-2 align-items-center mb-3 text-secondary small">
+            <div class="col-auto" style="font-size: 13px;"><span class="icon-calendar me-1"></span> <?= get_the_date('m/d/Y'); ?></div>
+            <div class="col-auto"><span class="d-block bg-secondary rounded-circle" style="width: 8px; height: 8px;"></span></div>
+            <div class="col-auto" style="font-size: 13px;"><span class="icon-folder me-1"></span><?= $cat; ?></div>
+        </div>
+
+        <h2 class="h5">
+            <a href="<?php the_permalink(); ?>" class="text-decoration-none">
+                <?= the_title(); ?>
+            </a>
+        </h2>
+
+        <a href="<?php the_permalink(); ?>" class="text-decoration-none small text-primary mt-2 text-limit">
+            <?= get_the_excerpt(); ?>
         </a>
-    </h2>
 
-    <div class="small text-primary mt-2 text-limit">
-        <?= get_the_excerpt(); ?>
     </div>
 
 </article>
