9738b8d62809e9ed2c07110dda62d54988196f1f
Brandão <lucas.brandao@planetaw.ag>
2025-10-15 12:31:33 -0300

    Ajuste icon espaçamento

--- themes/halley/template-parts/c-breadcrumbs.php
+++ themes/halley/template-parts/c-breadcrumbs.php
@@ -6,7 +6,7 @@
 
 
 <div class="d-inline-flex fs-6">
-    <i class="fa-solid fa-house me-1 text-primary" style="margin-top: 2px;"></i>
+    <i class="fa-solid fa-house me-2 text-primary" style="margin-top: 2px;"></i>
     <?php if (function_exists('yoast_breadcrumb')) {
         yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
     } ?>
