0b06102cd871c2235a2049277aaa114581db3840
Brandão <lucas.brandao@planetaw.ag>
2025-10-15 12:26:47 -0300

    Inserindo breadcrumbs

---
+++ themes/halley/template-parts/c-breadcrumbs.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Breadcrumbs plugin Yoast SEO
+ */
+?>
+
+
+<div class="d-inline-flex fs-6">
+    <i class="icon-home-2 me-1 text-primary" style="margin-top: 2px;"></i>
+    <?php if (function_exists('yoast_breadcrumb')) {
+        yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
+    } ?>
+</div>
