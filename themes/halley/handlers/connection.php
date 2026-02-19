ce33a9478ed9aac19441238d683486e9e416bd29
BrandsDev <lucasbrandao392@gmail.com>
2026-02-19 14:35:56 -0300

    feat: Inserindo dotenv para senhas e acessos

--- themes/halley/handlers/connection.php
+++ themes/halley/handlers/connection.php
@@ -3,15 +3,20 @@
 
 // ===== FUNÇÃO CONECTAR ======
 // ============================
+require_once __DIR__ . '/../lib/autoload.php'; // Certifique-se de que o autoload do Composer está disponível
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load();
+
 function conectar(){
-  $server = "halley_wp.mysql.dbaas.com.br";
-  $user   = "halley_wp";
-  $pass   = "LDX0K4wHT9q@";
-  $db     = "halley_wp";
+  $server = $_ENV['DB_HOST'];
+  $user   = $_ENV['DB_USER'];
+  $pass   = $_ENV['DB_PASS'];
+  $db     = $_ENV['DB_NAME'];
 
   try {
       $pdo = new PDO("mysql:host=$server;dbname=$db", $user, $pass );
-      $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); // EXIBIR ERROS
+      // $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); // EXIBIR ERROS
       $pdo->exec("SET CHARACTER SET utf8");
 
   } catch (\Throwable $th) {
--- themes/halley/handlers/util/email_configuracao.php
+++ themes/halley/handlers/util/email_configuracao.php
@@ -1,31 +1,37 @@
 <?php
 
+require_once __DIR__ . '/../../lib/autoload.php'; // Certifique-se de que o autoload do Composer está disponível
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../..');
+$dotenv->load();
+
 // NECESSÁRIO ESTAR ANTES DE TUDO
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 // CAMINHO PARA OS ARQUIVOS PHPMAILER BAIXADOS
-require '../../phpmailer/src/PHPMailer.php';
-require '../../phpmailer/src/SMTP.php';
-require '../../phpmailer/src/Exception.php';
-require_once '../../phpmailer/src/OAuth.php';
+require dirname(__FILE__).'/../../lib/phpmailer/src/PHPMailer.php';
+require dirname(__FILE__).'/../../lib/phpmailer/src/SMTP.php';
+require dirname(__FILE__).'/../../lib/phpmailer/src/Exception.php';
+require_once dirname(__FILE__).'/../../lib/phpmailer/src/OAuth.php';
 // ATUALIZAR AS INFORMAÇÕES ABAIXO BASEADO NO SERVIDOR DO EMAIL
 
 $mail = new PHPMailer(true);
 
 $mail->SMTPDebug = 0; // ATIVA O DEBUGGIN PARA VERIFICAÇÃO DE ERROS (remover quando finalizados os testes)
 $mail->IsSMTP(); // DEFINE O PADRÃO SMTP
-$mail->Host = "smtp-relay.brevo.com"; // HOST DO SMTP
-$mail->AuthType   = 'LOGIN';
+$mail->Host = $_ENV['EMAIL_HOST']; // HOST DO SMTP
 $mail->SMTPAuth = true;
-$mail->Username = "a28921001@smtp-brevo.com"; // EMAIL
-$mail->Password = "xsmtpsib-91bd1fdc882efbfa67fb201510ccf155d2623da508d4229c0de40c1e8231762e-DjBgy7UsOxdHidyv"; // SENHA
-$mail->SMTPSecure = "tls"; // SE O SMTP TIVER CRIPTOGRAFIA, USAR ESTA LINHA
-$mail->Port = 587; // PORTA
+$mail->Username = $_ENV['EMAIL_USER']; // EMAIL
+$mail->Password = $_ENV['EMAIL_PASS']; // SENHA
+$mail->SMTPSecure = $_ENV['EMAIL_SMTP']; // SE O SMTP TIVER CRIPTOGRAFIA, USAR ESTA LINHA
+$mail->Port = $_ENV['EMAIL_PORT']; // PORTA
 $mail->setFrom('contato@halleylog.com.br', 'Halley Log'); // MESMO EMAIL DO USERNAME
 
 $mail->addAddress($email); // EMAIL DO DESTINATÁRIO
+// $mail->AddBCC("lucas.brandao@planetaw.ag"); // EMAIL DO DESTINATÁRIO
+
 
 $mail->IsHTML(true); // DEFINE QUE O E-MAIL SERÁ ENVIADO COMO HTML
 $mail->setLanguage('pt');
